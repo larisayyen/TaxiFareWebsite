c93b95bafe89f332114ea6f785c697df5c952e04
larisa yen <larisayyen@yandex.com>
2022-04-29 11:55:16 +0800

    code ready to production

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from datetime import datetime,date,time
+import requests
+
+st.markdown('''# How much will next taxi fare be?
+## Input your destination. Let AI answer the question!
+''')
+
+date_ = st.date_input("Put your date",date(2022,4,29))
+time_ = st.time_input("Put your time",time(11,30))
+pickup_logitude = st.text_input("Put your pickup longitude")
+pickup_latitude = st.text_input("Put your pickup latitude")
+dropoff_longitude = st.text_input("Put your dropoff longitude")
+dropoff_latitude = st.text_input("Put your dropoff latitude")
+passenger_count = st.slider("Passenger count",1,9,2)
+
+
+url = 'https://taxifare.lewagon.ai/predict'
+
+if url == 'https://taxifare.lewagon.ai/predict':
+
+    st.markdown('API from lewagon')
+
+
+params = {
+    'pickup_datetime':datetime.combine(date_,time_),
+    'pickup_longitude':pickup_logitude,
+    'pickup_latitude':pickup_latitude,
+    'dropoff_longitude':dropoff_longitude,
+    'dropoff_latitude':dropoff_latitude,
+    'passenger_count':passenger_count
+}
+
+response = requests.get(url,params=params).json()
+
+st.json(response)
